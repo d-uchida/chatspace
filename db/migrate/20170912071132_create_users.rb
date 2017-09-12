92dc84b1cfaa959a6d00ba357da08f79deda173a
内田　大輔 <z15.uchida.daisuke@gmail.com>
2017-09-12 16:19:32 +0900

    Recreate users table
    user_idを消去

--- db/migrate/20170912071132_create_users.rb
+++ db/migrate/20170912071132_create_users.rb
@@ -1,10 +1,8 @@
 class CreateUsers < ActiveRecord::Migration[5.1]
   def change
     create_table :users do |t|
-      t.integer :user_id
       t.string :name
       t.string :email
-
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170912031017) do
+ActiveRecord::Schema.define(version: 20170912071132) do
 
-  create_table "users", force: :cascade do |t|
-    t.integer "user_id"
+  create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", null: false
