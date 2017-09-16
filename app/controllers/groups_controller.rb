6ee3c75bcbad405d7708d0adfd2ecbbc712f4d05
内田　大輔 <z15.uchida.daisuke@gmail.com>
2017-09-16 14:18:50 +0900

    Edit error message
    エラー数に応じて表示するエラーの数を変更するように修正

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -7,12 +7,10 @@ class GroupsController < ApplicationController
   end
 
   def create
-  	# render plain: params[@group].inspect
   	@group = Group.new(group_params)
     if @group.save
   	  redirect_to messages_path
     else
-      # render plain: @group.errors.inspect
       render "new"
     end
   end
