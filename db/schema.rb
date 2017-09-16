df8f889ad07166950d54b1cb4b817a527c30f59a
内田　大輔 <z15.uchida.daisuke@gmail.com>
2017-09-16 09:35:37 +0900

    Remove unnecessary column

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170915064255) do
+ActiveRecord::Schema.define(version: 20170915115011) do
 
   create_table "groups", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "name", null: false
-    t.integer "group_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
