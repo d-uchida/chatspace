26fe31841b0105a0a440f5b4a0b03d38590aa828
内田　大輔 <z15.uchida.daisuke@gmail.com>
2017-09-17 15:48:09 +0900

    Show group name in the left bar

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'messages#index'
+  root 'groups#new'
   resources :messages, only: [:index, :new, :create]
   resources :users, only: [:edit, :update]
   resources :groups, only: [:create, :new, :edit] do
-    resources :messages
+    resources :messages, :groups
   end
 end
