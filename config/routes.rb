8da1609cd8ab72a9618dc7248f1fd1dd93a350df
内田　大輔 <z15.uchida.daisuke@gmail.com>
2017-09-18 13:12:35 +0900

    Edit rooting to groups#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'groups#new'
+  root 'groups#index'
   resources :groups, only: [:index, :create, :new, :edit, :update] do
     resources :messages
   end
