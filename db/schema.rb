8b6ec12fa2889c7afe37280206f997ba8f43cfd9
内田　大輔 <z15.uchida.daisuke@gmail.com>
2017-09-19 14:03:15 +0900

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170918082248) do
+ActiveRecord::Schema.define(version: 20170918101140) do
 
   create_table "groups", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "name", null: false
@@ -19,7 +19,7 @@ ActiveRecord::Schema.define(version: 20170918082248) do
   end
 
   create_table "messages", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.text "body", null: false
+    t.text "text"
     t.string "image"
     t.string "table"
     t.integer "group_id"
