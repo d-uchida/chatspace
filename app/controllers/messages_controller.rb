f2b5915b18b2d1ae26c9cf027ff471ad718324a7
内田　大輔 <z15.uchida.daisuke@gmail.com>
2017-09-19 14:06:56 +0900

    Delete unnecessary lines

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,7 +1,7 @@
 class MessagesController < ApplicationController
 
   def index
-    @group = Group.find(params[:group_id])
+    @message = Message.new
     @groups = current_user.groups
   end
 
@@ -9,12 +9,18 @@ class MessagesController < ApplicationController
     @message = Message.new
   end
 
-  def create
-    Message.create(params[:table])
+  def show
+    @messages = Messages.find(params[:id])
   end
 
-def message_params
-end
+  def create
+    Message.create(text: params[:text], image: params[:image], group_id: params[:group_id], user_id: current_user.id)
+    redirect_to group_messages_path(:group_id)
+  end
 
+  private
+  def message_params
+    params.permit(:text, :image, :group_id)
+  end
 
 end
