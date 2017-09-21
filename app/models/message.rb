a470807f55700ef67d2dc1137ab1b95d12139791
内田　大輔 <z15.uchida.daisuke@gmail.com>
2017-09-21 21:06:04 +0900

    Change validation from "text, presence true" to "text or image, presence true"

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,5 +2,10 @@ class Message < ApplicationRecord
   belongs_to :user
   belongs_to :group
   mount_uploader :image, ImageUploader
-  validates :text, presence: true
+  validates :text_or_image, presence: true
+
+  private
+    def text_or_image
+    	text.presence or image.presence
+    end
 end
