e90e94e6af495edddb21b8927fffde83f2de4073
内田　大輔 <z15.uchida.daisuke@gmail.com>
2017-09-23 14:20:07 +0900

    Add method to allow certain type of image to upload

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -3,6 +3,11 @@ class ImageUploader < CarrierWave::Uploader::Base
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
+
+  def extension_white_list
+    %w(jpg jpeg)
+  end
+
   include CarrierWave::MiniMagick
   process resize_to_limit: [400, 200]
 end
