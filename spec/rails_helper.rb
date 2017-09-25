01f23d99c8921921df71655ec64509228e309fad
内田　大輔 <z15.uchida.daisuke@gmail.com>
2017-09-25 17:53:56 +0900

    Set up rails_helper.rb to reflect image files

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -26,14 +26,21 @@ require 'rspec/rails'
 # If you are not using ActiveRecord, you can remove this line.
 ActiveRecord::Migration.maintain_test_schema!
 
+
+require 'devise'
+require File.expand_path("spec/support/controller_macros.rb")
+
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
-
+  config.include FactoryGirl::Syntax::Methods
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
   config.use_transactional_fixtures = true
+
+  config.include Devise::TestHelpers, type: :controller
+  config.include ControllerMacros, type: :controller
 
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
