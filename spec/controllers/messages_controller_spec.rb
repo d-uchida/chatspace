dd48afb210991371232cfc85d2df010e5b38b9fc
内田　大輔 <z15.uchida.daisuke@gmail.com>
2017-09-27 15:59:37 +0900

    Refactor messages_controller_spec.rb

--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -9,34 +9,29 @@ describe MessagesController, type: :controller do
   let(:image) { create( :image ) }
 
   describe 'GET #index' do
-    before do
-      login_user user
+    context "when user log in" do 
+      before do
+        login_user user
+        get :index, params: { group_id: group }
+      end
+      it "assigns the requested contact to @groups" do
+        @group = user.groups.first
+        groups = user.groups
+        expect(assigns(:groups)).to eq groups
+      end
+      it "renders the :index template" do
+        expect(response).to render_template :index
+      end
     end
-
-    it "assigns the requested contact to @groups" do
-      @group = user.groups.first
-      get :index, params: { group_id: group }
-      groups = user.groups
-      expect(assigns(:groups)).to eq groups
-    end
-  end
-  
-  describe 'GET #index' do
-    before do
-      login_user user
-    end
-
-    it "renders the :index template" do
-      get :index, params: { group_id: group }
-      expect(response).to render_template :index
-    end
-  end
-
-
-  describe 'GET #index' do
-    it "renders login view" do
-      get :index
-      expect(response).to redirect_to new_user_session_path
+    context "when user NOT log in" do
+      it "renders login view" do
+        get :index
+        expect(response).to redirect_to new_user_session_path
+      end
+      it "renders the :index template" do
+        get :index, params: { group_id: group }
+        expect(response).to redirect_to new_user_session_path
+      end
     end
   end
 
@@ -49,45 +44,19 @@ describe MessagesController, type: :controller do
         post :create, params: { message: attributes_for(:message), group_id: group }
       end.to change(Message, :count).by(1)
     end
-  end
-
-  describe 'POST #create' do
-    before do
-      login_user user
-    end
     it "renders message index" do
       post :create, params: { message: attributes_for(:message), group_id: group}
-
       expect(response).to redirect_to group_messages_path
-
-    end
-  end
-
-  describe 'POST #create' do
-    before do
-      login_user user
     end
     it "couldn't save the message" do
       expect do
         post :create, params: { message: {text: "", image: ""}, group_id: group}
       end.to change(Message, :count).by(0)
     end
-  end
 
-  describe 'POST #create' do
-    before do
-      login_user user
-    end
     it "moves to renders index" do
       post :create, params: { message: {text: "", image: ""}, group_id: group}
       expect(response).to render_template(:index)
-    end
-  end
-
-  describe 'GET #index' do
-    it "renders the :index template" do
-      get :index, params: { group_id: group }
-      expect(response).to redirect_to new_user_session_path
     end
   end
 end
