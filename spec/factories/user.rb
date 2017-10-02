15013cec27325922e63a2e542603d1430057eb22
内田　大輔 <z15.uchida.daisuke@gmail.com>
2017-10-02 16:44:09 +0900

    Edit factory girl

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,20 +1,19 @@
-# FactoryGirl.define do
-#   pass = Faker::Internet.password(8)
+FactoryGirl.define do
 
-#   factory :user do
-#     name                   Faker::Name.name
-#     email                  Faker::Internet.email
-#     password              pass
-#     password_confirmation pass
+  factory :user do
+    name                  {Faker::Name.name}
+    email                 {Faker::Internet.email}
+    password              {Faker::Internet.password(8)}
+    password_confirmation {password}
 
-#     # after(:create) do |user|
-#     #   temp_group = create(:group)
-#     #   create(:message, user: user, group: temp_group)
-#     #   create(:user_group, user: user, group: temp_group)
-#     # end
+    # after(:create) do |user|
+    #   temp_group = create(:group)
+    #   create(:message, user: user, group: temp_group)
+    #   create(:user_group, user: user, group: temp_group)
+    # end
 
-#     # after(:create) do |user|
-#     #   create(:user_group, user: user, group: create(:group))
-#     # end
-#   end
-# end
+    # after(:create) do |user|
+    #   create(:user_group, user: user, group: create(:group))
+    # end
+  end
+end
