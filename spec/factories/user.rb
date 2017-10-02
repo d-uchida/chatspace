26420e35329db9fb41ed0eca594c97ad57df188e
内田　大輔 <z15.uchida.daisuke@gmail.com>
2017-10-02 13:10:49 +0900

    Freeze user.rb for testing
    この記載があるとローカルサーバーが立ち上がらないためコメントアウト

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,20 +1,20 @@
-FactoryGirl.define do
-  pass = Faker::Internet.password(8)
+# FactoryGirl.define do
+#   pass = Faker::Internet.password(8)
 
-  factory :user do
-    name                   Faker::Name.name
-    email                  Faker::Internet.email
-    password              pass
-    password_confirmation pass
+#   factory :user do
+#     name                   Faker::Name.name
+#     email                  Faker::Internet.email
+#     password              pass
+#     password_confirmation pass
 
-    # after(:create) do |user|
-    #   temp_group = create(:group)
-    #   create(:message, user: user, group: temp_group)
-    #   create(:user_group, user: user, group: temp_group)
-    # end
+#     # after(:create) do |user|
+#     #   temp_group = create(:group)
+#     #   create(:message, user: user, group: temp_group)
+#     #   create(:user_group, user: user, group: temp_group)
+#     # end
 
-    # after(:create) do |user|
-    #   create(:user_group, user: user, group: create(:group))
-    # end
-  end
-end
+#     # after(:create) do |user|
+#     #   create(:user_group, user: user, group: create(:group))
+#     # end
+#   end
+# end
