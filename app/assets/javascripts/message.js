b3e2f3fb6b62c8ac03da19f7e9e76485fff8d828
内田　大輔 <z15.uchida.daisuke@gmail.com>
2017-10-05 20:28:49 +0900

    Edit message.js for auto update

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,9 +1,8 @@
 $(function(){
   function buildHTML(message){
-      if (message.image.url !== "") {
-        insertImage = `<img src="${message.image.url}">`;
-    }
-    var html = `
+    // insertImage = `<img src="${message.image.url}">`;
+     if (message.text !== ""&&message.image.url !== null) {
+        var html = `
                 <div class="right-messages__middle--user" data-message-id="${message.id}">
                   <div class="right-messages__middle--user__list">
                     ${message.name}
@@ -12,10 +11,41 @@ $(function(){
                     </span>
                   </div>
                   <div class="right-messages__middle--message">${message.text}</div>
-                  ${insertImage}
+                  <img src="${message.image.url}">
                 </div>`
-    return html;
+        return html;
+
+      } else if (message.text !== "" && message.image.url == null){
+      var html = `
+                <div class="right-messages__middle--user" data-message-id="${message.id}">
+                  <div class="right-messages__middle--user__list">
+                    ${message.name}
+                    <span>
+                      ${message.created_at}
+                    </span>
+                  </div>
+                  <div class="right-messages__middle--message">${message.text}</div>
+                </div>`
+       return html;
+
+      }  else if (message.text == "" && message.image.url !== null){
+      var html = `
+                <div class="right-messages__middle--user" data-message-id="${message.id}">
+                  <div class="right-messages__middle--user__list">
+                    ${message.name}
+                    <span>
+                      ${message.created_at}
+                    </span>
+                  </div>
+                  <div class="right-messages__middle--message">${message.text}</div>
+                  <img src="${message.image.url}">
+                </div>`
+       return html;
+    } else {
+
+    }
 }
+
 $('#new_message').on('submit', function(e) {
   e.preventDefault();
   var formData = new FormData($(this).get(0));
@@ -38,6 +68,37 @@ $('#new_message').on('submit', function(e) {
   })
   .fail(function(){
     alert('error');
-  })  
-})
+  })
+});
+
+var auto_update = function(){
+  if (location.href.match(/\/groups\/\d+\/messages/)) {
+      $.ajax({
+        url: location.href,
+        type: 'GET',
+        dataType: 'json'
+      })
+      
+      .done(function(messages) {
+        var latest_id = $("#msg > div:last").data('messageId');
+        var insertHTML = '';
+        messages.forEach(function(message) {
+          if (message.id > latest_id) {
+            insertHTML += buildHTML(message);
+          }
+        });
+        $('#msg').append(insertHTML);
+        $('.right-messages').animate({scrollTop: $("#msg")[0].scrollHeight}, 'slow');
+        console.log("自動更新に成功しました");
+      })
+      .fail(function(messages) {
+        alert('自動更新に失敗しました');
+      });
+    } else {
+      clearInterval(interval);
+  }
+}
+
+setInterval(auto_update, 5 * 1000 );
+
 });
